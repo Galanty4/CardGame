fd857d969c2ea556f23f8983e80146fc91f5325c
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2021-11-15 20:26:22 +0100

    card back added with animation

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -7,7 +7,7 @@ import GameCard from './components/Card/GameCard';
 function App() {
   return (
     <div style={{ margin: 64 }}>
-      <GameCard imgSrc="/placeholder2.jpg" w={200} h={315} />
+      <GameCard imgSrc="/placeholder2.jpg" w={200} h={315} description={<><b>Test</b>: Fireball Enemies</>} />
     </div>
   );
 }
--- ClientApp/src/components/Card/GameCard.tsx
+++ ClientApp/src/components/Card/GameCard.tsx
@@ -1,4 +1,5 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
 
 import './GameCard.less';
 
@@ -11,26 +12,40 @@ export interface IGameCard {
 }
 const GameCard: React.FC<IGameCard> = (props) => {
   const { w, h, imgSrc, description, spellPower = 0 } = props;
+  const [flipped, setIsFlipped] = useState<boolean>(false);
 
   return (
-    <div className="game-card" style={{ width: w, height: h }}>
-      <div className="game-card__spellpower-container">
-        <div className="game-card__spellpower">
-          <b>
-            {spellPower}
-          </b>
+    <ReactCardFlip isFlipped={flipped} flipDirection="vertical">
+      <div onClick={() => setIsFlipped(true)} className="game-card game-card--front" style={{ width: w, height: h }}>
+        <div className="game-card__spellpower-container">
+          <div className="game-card__spellpower">
+            <b>
+              {spellPower}
+            </b>
+          </div>
+        </div>
+        <div className="game-card__image-container">
+          <div style={{ backgroundImage: `url(${imgSrc})` }} className="game-card__image-bg" >
+          </div>
+        </div>
+        <div className="game-card__description">
+          <div className="game-card__description-text">
+            {description}
+          </div>
         </div>
       </div>
-      <div className="game-card__image-container">
-        <div style={{ backgroundImage: `url(${imgSrc})` }} className="game-card__image-bg" >
+
+      <div onClick={() => setIsFlipped(false)} className="game-card game-card--back" style={{ width: w, height: h }}>
+        <div className="game-card__back-circle"></div>
+        <div className="game-card__back-row">
+          <div className="game-card__back-col"></div>
+          <div className="game-card__back-col"></div>
+          <div className="game-card__back-col"></div>
+          <div className="game-card__back-col"></div>
         </div>
       </div>
-      <div className="game-card__description">
-        <div className="game-card__description-text">
-          <b>Test</b>: Fireball Enemies
-        </div>
-      </div>
-    </div>
+
+    </ReactCardFlip>
   );
 }
 
