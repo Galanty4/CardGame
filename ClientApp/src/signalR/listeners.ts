43bc77da9c8dbb560d2b0783693a198e12b1d2ff
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2021-11-26 20:15:30 +0100

    refactor connection

--- ClientApp/src/signalR/listeners.ts
+++
@@ -1,8 +0,0 @@
-import { connection } from ".";
-import { store } from "../store";
-import { updateMessages } from "../store/user/actions";
-
-connection.on("ReceiveMessage", (user, message) => {
-  const messages = store.getState().userReducer.session.messages;
-  store.dispatch(updateMessages([...messages, {user, message}]))
-});
