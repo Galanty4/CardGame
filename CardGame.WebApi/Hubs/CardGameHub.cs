b9807d40ef996c096b116b563cc524b93e8e027d
Igor Kowalewski <kowalewski.igor.ug@gmail.com>
2021-11-27 19:38:31 +0100

    Fixed UserConnected

--- CardGame.WebApi/Hubs/CardGameHub.cs
+++ CardGame.WebApi/Hubs/CardGameHub.cs
@@ -22,8 +22,12 @@ namespace CardGame.WebApi.Hubs
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
 
+            _connections[Context.ConnectionId] = userConnection;
+
             await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser, 
                 $"{userConnection.User} has joined {userConnection.Room}");
+
+            await SendUsersConnected(userConnection.Room);
         }
 
         public async Task SendMessage(string message)
