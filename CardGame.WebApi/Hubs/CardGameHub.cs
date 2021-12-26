0ce5b3c4db1d165fbb6c81cd9071a8f013df2b44
Igor Kowalewski <kowalewski.igor.ug@gmail.com>
2021-12-26 12:42:19 +0100

    Game Async Methods - Position, Attack, Turn, State of Card

--- CardGame.WebApi/Hubs/CardGameHub.cs
+++ CardGame.WebApi/Hubs/CardGameHub.cs
@@ -20,6 +20,13 @@ namespace CardGame.WebApi.Hubs
 
         public async Task JoinRoom(UserConnection userConnection)
         {
+            if (_connections.Sum(x => x.Value.Room == userConnection.Room ? 1 : 0) >= 2) // drewniana walidacja ilości użytkowników w grupie
+            {
+                await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser,
+                $"{userConnection.User} was trying to join {userConnection.Room}");
+                return;
+            }
+
             await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
 
             _connections[Context.ConnectionId] = userConnection;
@@ -33,9 +40,43 @@ namespace CardGame.WebApi.Hubs
         public async Task SendMessage(string message)
         {
             if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
-            {
                 await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", userConnection.User, message);
-            }
+        }
+
+        public async Task ChangeCardPosition(int playerCard, int[] position)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+                await Clients.OthersInGroup(userConnection.Room).SendAsync("ReceiveCardPosition", userConnection.User, playerCard, position);
+        }
+
+        public async Task PlayerAttack(int player1Card, int player2Card)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+                await Clients.Group(userConnection.Room).SendAsync("ReceivePlayerAttack", userConnection.User, player1Card, player2Card);
+        }
+
+        public async Task EndTurn(int turn)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+                await Clients.Group(userConnection.Room).SendAsync("ReceiveTurnChange", userConnection.User, turn);
+        }
+
+        public async Task GetCard(int playerDeckCard)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+                await Clients.Group(userConnection.Room).SendAsync("ReceiveCardFromDeck", userConnection.User, playerDeckCard);
+        }
+
+        public async Task ActivateCard(int playerCard, int state)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+                await Clients.Group(userConnection.Room).SendAsync("ReceiveActivateCard", userConnection.User, playerCard, state);
+        }
+
+        public async Task DeactivateCard(int playerCard, int state)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+                await Clients.Group(userConnection.Room).SendAsync("ReceiveDeactivateCard", userConnection.User, playerCard, state);
         }
 
         public override Task OnDisconnectedAsync(Exception exception)
